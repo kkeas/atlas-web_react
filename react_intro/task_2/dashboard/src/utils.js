b2b61c17e5e2b2af9cad2d1eee36613e63e830b8
kkeas <keaskatrina@gmail.com>
2024-05-07 10:54:33 -0500

    task 2 no styling though bc task did not ask for it

---
+++ react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,14 @@
+
+
+function getFullYear() {
+    return new Date().getFullYear();
+  }
+  
+  function getFooterCopy(isIndex) {
+    if (isIndex) {
+      return 'Holberton School';
+    }
+    return 'Holberton School main dashboard';
+  }
+  
+  export { getFullYear, getFooterCopy };
