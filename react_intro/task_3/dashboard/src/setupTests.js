4212eb07f8830d19a5ef5c7f79b494d728acf2f2
kkeas <keaskatrina@gmail.com>
2024-05-07 12:23:46 -0500

    tests are passing

--- react_intro/task_3/dashboard/src/setupTests.js
+++ react_intro/task_3/dashboard/src/setupTests.js
@@ -3,7 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
+
 
 Enzyme.configure({ adapter: new Adapter() });
+
